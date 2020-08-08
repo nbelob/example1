544fae384cc744167b688a41d0c1c39708350f96
bnl <b_n_l@mail.ru>
2020-08-08 18:07:44 +0300

    Initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+//import { AppComponent } from './app.component';
+import { ProductComponent } from "./component";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ProductComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProductComponent]
 })
 export class AppModule { }
