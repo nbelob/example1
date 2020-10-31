c89d3aab184182f881328fc8d583482766a77d58
bnl <b_n_l@mail.ru>
2020-10-31 17:33:44 +0300

    Using Events and Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 // import { AppComponent } from './app.component';
 import { ProductComponent } from './component';
 import { AppComponent } from './app.component';
 @NgModule({
   declarations: [ProductComponent, AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [ProductComponent]
 })
